3275d86ed5e93f8c5117b92e5f8ff9c7a6ab1b65
Potopilo404 <potopilo123@hotmail.com>
2025-01-04 19:22:51 +0100

    Update start.bat
    Correction bug

--- start.bat
+++ start.bat
@@ -1,19 +1,19 @@
 @echo off
 
-echo "@echo Off
+echo @echo Off
 
-echo "Welcome Discord Hacker! Anything you do with this tool will be done of your own free will. We are not responsible for what happens to you.  This tool is in full development."
+echo Welcome Discord Hacker! Anything you do with this tool will be done of your own free will. We are not responsible for what happens to you.  This tool is in full development.
 
 
 
-set /p answer="Do you want to continue (Y/N)? "
+set /p answer=Do you want to continue (Y/N)? 
 
 if /i "%answer%"=="Y" (
-  echo "You chose to continue."
+  echo You chose to continue.
 ) else if /i "%answer%"=="N" (
-  echo "You chose to cancel."
+  echo You chose to cancel.
 ) else (
-  echo "Invalid input."
+  echo Invalid input.
 )
 
 timeout /t 5 /nobreak
