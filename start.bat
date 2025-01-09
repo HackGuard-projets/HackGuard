6d6b1363bd7dd466816450ec39bbbabab24ae91e
Potopilo404 <potopilo123@hotmail.com>
2025-01-09 20:35:34 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,21 +1,9 @@
 @echo off
 
-echo @echo Off
-
-echo Welcome Discord Hacker! Anything you do with this tool will be done of your own free will. We are not responsible for what happens to you.  This tool is in full development.
-
-
-
 set /p answer=Do you want to continue (Y/N)? 
 
 if /i "%answer%"=="Y" (
-  echo You chose to continue.
-) else if /i "%answer%"=="N" (
-  echo You chose to cancel.
-) else (
-  echo Invalid input.
+  python main.py
 )
 
 timeout /t 5 /nobreak
-
-python main.py
