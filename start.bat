67ffee078f4d19fa486958d41eba4b59012baaa3
Potopilo404 <potopilo123@hotmail.com>
2025-01-17 16:01:44 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,6 +4,10 @@ set /p answer=Do you want to continue (Y/N)?
 
 if /i "%answer%"=="Y" (
   python main.py
+  if errorlevel 1 (
+    echo "python a échoué, tentative avec py..."
+    py main.py
+  )
 )
 
 timeout /t 5 /nobreak
