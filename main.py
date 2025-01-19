3bc68e3635453363b3bc025a3cc03e07f20e8674
Potopilo404 <potopilo123@hotmail.com>
2025-01-19 16:08:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import fade
 from colorama import Fore, Style, init
 
 init(autoreset=True)
@@ -9,13 +10,13 @@ def clear_console():
 
 def display_main_menu():
     clear_console()
-    print(Fore.BLUE + r"""
+    ascii_art = """
              _    _            _     _____                     _ 
             | |  | |          | |   / ____|                   | |
             | |__| | __ _  ___| | _| |  __ _   _  __ _ _ __ __| |
             |  __  |/ _` |/ __| |/ / | |_ | | | |/ _` | '__/ _` |
             | |  | | (_| | (__|   <| |__| | |_| | (_| | | | (_| |
-            |_|  |_|\__,_|\___|_|\_\\_____|\__,_|\__,_|_|  \__,_| (By potopilo)
+            |_|  |_|\__,_|\___|_|\_\\______|\__,_|\__,_|_|  \__,_| (By potopilo)
 
    ┌─────────────────────────────── File and System Tools ───────────────────────────────┐
    │ [1] File Hash Checker                     [2] Network Traffic Monitor               │
@@ -32,7 +33,28 @@ def display_main_menu():
    │ [17] Phishing Detection Tool              [18] Browser Fingerprinting Tool          │
    │ [19] Credential Stuffing Tool             [20] API Testing Tool                     │
    └─────────────────────────────────────────────────────────────────────────────────────┘
+   ┌─────────────────────────────────────────────────────────────────────────────────────┐
+   │ Press (B) to go Back                                             Press (N) for Next │
+   └─────────────────────────────────────────────────────────────────────────────────────┘
+   """
+    clear_console()
+    print(fade.purplepink(ascii_art))
 
+def display_next_menu():
+    clear_console()
+    menu2()
+
+
+def menu2():
+    clear_console()
+    ascii_art2 = """
+             _    _            _     _____                     _ 
+            | |  | |          | |   / ____|                   | |
+            | |__| | __ _  ___| | _| |  __ _   _  __ _ _ __ __| |
+            |  __  |/ _` |/ __| |/ / | |_ | | | |/ _` | '__/ _` |
+            | |  | | (_| | (__|   <| |__| | |_| | (_| | | | (_| |
+            |_|  |_|\__,_|\___|_|\_\\______|\__,_|\__,_|_|  \__,_| (By potopilo)
+            
    ┌─────────────────────────────── Data and Recovery Tools ─────────────────────────────┐
    │ [21] Data Breach Checker                  [22] File Integrity Checker               │
    │ [23] RAT Simulator                        [24] WAF Tester                           │
@@ -51,37 +73,51 @@ def display_main_menu():
 
    ┌─────────────────────────────────────────────────────────────────────────────────────┐
    │ Press (B) to go Back                                             Press (N) for Next │
-   └─────────────────────────────────────────────────────────────────────────────────────┘
-"""+ Style.RESET_ALL)
+   └─────────────────────────────────────────────────────────────────────────────────────┘"""
+    print(fade.purplepink(ascii_art2))
+ 
 
-
-def display_next_menu():
-    clear_console()
-    print("No other menu for moment !")
 
 def main():
     current_menu = 'main'
-
+    current_menu2 = 'main2'
     while True:
         if current_menu == 'main':
             display_main_menu()
-            choice = input(Fore.YELLOW + "Choose an option (1-40, N for Next, B for Back): " + Style.RESET_ALL)
+            choice = input(Fore.LIGHTMAGENTA_EX+"Choose an option (1-20) : ")
 
             if choice == 'N' or choice.lower() == 'n':
-                current_menu = 'next'
+                menu2()
             elif choice == 'B' or choice.lower() == 'b':
-                print(Fore.RED + "You are already in the main menu.")
+                display_main_menu()
             elif choice == '0':
                 print(Fore.RED + "Goodbye!")       
                 break
             elif choice in [str(i) for i in range(1, 30)]: 
                 subprocess.run(["python", os.path.join(os.path.dirname(__file__), "spring", f"tool_{choice}.py")])
             else:
-                print(Fore.GREEN + "Invalid choice. Please enter a number between 1 and 29.")            
+                print(Fore.GREEN + "Invalid choice. Please enter a number between 1 and 40.")
+                
+            if current_menu == 'main2':
+               menu2()
+            choice = input(Fore.LIGHTMAGENTA_EX+"Choose an option (21-40) : ")
+
+            if choice == 'N' or choice.lower() == 'n':
+                clear_console()
+                input("No next menu.")
+            elif choice == 'B' or choice.lower() == 'b':
+                display_main_menu()
+            elif choice == '0':
+                print(Fore.RED + "Goodbye!")       
+                break
+            elif choice in [str(i) for i in range(1, 30)]: 
+                subprocess.run(["python", os.path.join(os.path.dirname(__file__), "spring", f"tool_{choice}.py")])
+            else:
+                print(Fore.GREEN + "Invalid choice. Please enter a number between 1 and 40.")
+                        
 
         elif current_menu == 'next':
-            display_next_menu()
-            choice = input(Fore.YELLOW + "No menu (press B or b) " + Style.RESET_ALL)
+            menu2()
 
             if choice == 'B' or choice.lower() == 'b':
                 current_menu = 'main'
@@ -91,6 +127,10 @@ def main():
                 subprocess.run(["python", os.path.join(os.path.dirname(__file__), "spring", f"tool_{choice}.py")])
             else:
                 print(Fore.GREEN + "Invalid choice. Please enter a number between 22 and 29.")
+                
+                
+
+
 
 if __name__ == "__main__":
     main()
